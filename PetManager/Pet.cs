e47a80d73748c644b46086b265979dadf2cde982
Fabian <fabi.lengwiler@bluewin.ch>
2018-08-25 14:20:07 +0200

    Commit Teammitglied 2

--- PetManager/Pet.cs
+++ PetManager/Pet.cs
@@ -8,5 +8,15 @@ namespace PetManager {
     public class Pet {
         public string Name { get; set; }
         public string Breed { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Age
+        {
+            get { return (DateTime.Now - Birthday).Days / 365; }
+        }
+
+        public Pet()
+        {
+            Birthday = new DateTime(2010, 10, 10);
+        }
     }
 }
